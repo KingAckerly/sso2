00b42f48f777d091d9618f63d92f4e2af966315a
Carl <1931931123@qq.com>
2021-07-15 10:41:51 +0800

    sso2

---
+++ src/main/java/com/lsm/sso2/Sso2Application.java
@@ -0,0 +1,13 @@
+package com.lsm.sso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sso2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sso2Application.class, args);
+    }
+
+}
